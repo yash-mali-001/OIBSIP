423620df1f7c9009f6df6413bf0f5b6e1fbf6c0a
yash-mali-001 <yashmalighub45@gmail.com>
2024-01-10 00:58:29 +0530

    First Commit

---
+++ NumberGuessingGame.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class NumberGuessingGame{
+    public static void main(String agrs[]){
+        int max  , random_num , user_num ;
+        Random r = new Random();
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter Upper limit");
+        max = sc.nextInt();
+        random_num = r.nextInt(max)+1;
+        while (true) {
+            System.out.println(random_num);
+            System.out.println("Enter Your Guess : ");
+            user_num = sc.nextInt();
+            if(random_num == user_num){
+                System.out.println("Ohhh! Niceeee , You Guessed it right");
+                break;
+            }
+            else if(user_num > random_num){
+                System.out.println("Number is lower that this.");
+            }
+            else{
+                System.out.println("Number is Higher that this.");
+            }
+        }
+        sc.close();
+        System.out.println("End!");
+    }  
+}
